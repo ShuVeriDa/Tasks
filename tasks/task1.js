f36a8e2cbeff222b535700505ff1cc5b085d075f
shuverida <bashtarov@outlook.com>
2023-10-25 11:22:39 +0300

    First Task completed

---
+++ tasks/task1.js
@@ -0,0 +1,16 @@
+const isPalindrome = (str) => {
+  // Строка приводится к нижнему регистру и удаляются символы.
+  const value = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+
+  // Создаем обратную копию
+  const reversedString = value.split('').reverse().join('')
+
+  // Сравниваем оригинал с обратным
+  return value === reversedString
+}
+const valuePalindrome = "аргентина манит негра";
+
+// Проверяем
+isPalindrome(valuePalindrome)
+  ? console.log("Это палиндром.")
+  : console.log("Это не палиндром.");
